6877a70d45e25b6e68305079f6c052b46660ae57
Dan Wahl <drwahl@gmail.com>
2018-02-10 18:56:39 -0600

    updated db testing

--- db/create_db.sql
+++ db/create_db.sql
@@ -21,6 +21,25 @@ CREATE DATABASE IF NOT EXISTS elpc_air_quality
 USE elpc_air_quality;
 
 --
+-- Drop table "users"
+--
+DROP TABLE IF EXISTS users;
+
+--
+-- Create table "users"
+--
+CREATE TABLE users (
+  id INT(11),
+  username VARCHAR(40) NOT NULL,
+  PRIMARY KEY (username),
+  INDEX index_users_on_id (id)
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+ROW_FORMAT = DYNAMIC;
+
+--
 -- Drop table "sessions"
 --
 DROP TABLE IF EXISTS sessions;
@@ -30,12 +49,152 @@ DROP TABLE IF EXISTS sessions;
 --
 CREATE TABLE sessions (
   id INT(11) NOT NULL,
-  username VARCHAR(40) NOT NULL,
-  start_time_local DATE NOT NULL,
-  end_time_local DATE NOT NULL,
-  title VARCHAR(40) NOT NULL,
+  created_at DATE,
+  updated_at DATE,
+  user_id INT,
+  uuid VARCHAR(40),
+  url_token VARCHAR(40),
+  title TEXT,
+  description TEXT,
+  calibration INT,
+  contribute BOOLEAN,
+  data_type VARCHAR(40),
+  instrument VARCHAR(40),
+  phone_model VARCHAR(40),
+  os_version VARCHAR(40),
+  offset_60_db VARCHAR(40),
+  start_time DATE,
+  end_time DATE,
+  measurements_count INT,
+  timezone_offset INT,
+  start_time_local DATE,
+  end_time_local DATE,
   type VARCHAR(40) NOT NULL,
-  PRIMARY KEY (id)
+  is_indoor BOOLEAN,
+  latitude DOUBLE,
+  longitude DOUBLE,
+  last_measurement_at DATE,
+  PRIMARY KEY (id),
+  INDEX index_sessions_on_contribute (contribute),
+  INDEX index_sessions_on_end_time (end_time),
+  INDEX index_sessions_on_local_end_time (end_time_local),
+  INDEX index_sessions_on_last_measurement_at (last_measurement_at),
+  INDEX index_sessions_on_start_time (start_time),
+  INDEX index_sessions_on_local_start_time (start_time_local),
+  INDEX index_sessions_on_url_token (url_token),
+  INDEX index_sessions_on_user_id (user_id),
+  INDEX index_sessions_on_uuid (uuid),
+  CONSTRAINT constrain_session_on_user_id FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+ROW_FORMAT = DYNAMIC;
+
+--
+-- Drop table "notes"
+--
+DROP TABLE IF EXISTS notes;
+
+--
+-- Create table "notes"
+--
+CREATE TABLE notes (
+  id INT(11) NOT NULL,
+  created_at DATE,
+  updated_at DATE,
+  date DATE,
+  text TEXT,
+  latitude DOUBLE,
+  longitude DOUBLE,
+  session_id INT(11),
+  photo_file_name VARCHAR(40),
+  photo_content_type VARCHAR(40),
+  photo_file_size INT(11),
+  photo_updated_at DATE,
+  number INT(11),
+  PRIMARY KEY (id, session_id),
+  INDEX index_notes_on_session_id (session_id),
+  CONSTRAINT constrain_notes_on_session_id FOREIGN KEY (session_id)
+    REFERENCES sessions(id) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+ROW_FORMAT = DYNAMIC;
+
+--
+-- Drop table "streams"
+--
+DROP TABLE IF EXISTS streams;
+
+--
+-- Create table "streams"
+--
+CREATE TABLE streams (
+  id INT(11) NOT NULL,
+  sensor_name VARCHAR(40),
+  unit_name VARCHAR(40),
+  measurement_type VARCHAR(40),
+  measurement_short_type VARCHAR(40),
+  unit_symbol VARCHAR(40),
+  threshold_very_low INT(11),
+  threshold_low INT(11),
+  threshold_medium INT(11),
+  threshold_high INT(11),
+  threshold_very_high INT(11),
+  session_id INT(11),
+  sensor_package_name VARCHAR(40) DEFAULT "Builtin" NOT NULL,
+  measurements_count INT(11) DEFAULT 0 NOT NULL,
+  min_latitude DOUBLE,
+  max_latitude DOUBLE,
+  min_longitude DOUBLE,
+  max_longitude DOUBLE,
+  average_value FLOAT,
+  PRIMARY KEY (id),
+  INDEX index_streams_on_max_latitude (max_latitude),
+  INDEX index_streams_on_max_longitude (max_longitude),
+  INDEX index_streams_on_min_latitude (min_latitude),
+  INDEX index_streams_on_min_longitude (min_longitude),
+  INDEX index_streams_on_sensor_name_and_measurement_type (sensor_name, measurement_type),
+  INDEX index_streams_on_sensor_name (sensor_name),
+  INDEX index_streams_on_session_id (session_id),
+  CONSTRAINT constrain_streams_on_session_id FOREIGN KEY (session_id)
+    REFERENCES sessions(id) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+ROW_FORMAT = DYNAMIC;
+
+--
+-- Drop table "measurements"
+--
+DROP TABLE IF EXISTS measurements;
+
+--
+-- Create table "measurements"
+--
+CREATE TABLE measurements (
+  id INT(11) NOT NULL,
+  value FLOAT,
+  latitude DOUBLE,
+  longitude DOUBLE,
+  time DATE,
+  timezone_offset INT(11),
+  stream_id INT(11),
+  milliseconds INT(11) DEFAULT 0,
+  measured_value FLOAT,
+  created_at DATE,
+  PRIMARY KEY (id, stream_id),
+  INDEX index_measurements_on_latitude (latitude),
+  INDEX index_measurements_on_longitude_and_latitude (latitude, longitude),
+  INDEX index_measurements_on_longitude (longitude),
+  INDEX index_measurements_on_stream_id (stream_id),
+  INDEX index_measurements_on_time (time),
+  CONSTRAINT constrain_measurements_on_stream_id FOREIGN KEY (stream_id)
+    REFERENCES streams(id) ON DELETE CASCADE ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
--- db/db_test.py
+++ db/db_test.py
@@ -6,20 +6,64 @@ if __name__ == '__main__':
     # create engine
     engine = sa.create_engine('mysql+mysqlconnector://root:dd00ww@home.danwahl.net/elpc_air_quality')
     
-    # api quiery
-    params = (
-        ('q[usernames]', 'JustDesign'),
-    )
-    response = requests.get('http://aircasting.org/api/sessions.json', params=params)
+    # get users
+    users = pd.read_sql_query('SELECT * FROM users', engine, index_col='id')
     
-    # sessions dataframe
-    sessions = pd.DataFrame(response.json())
-    sessions['start_time_local'] = pd.to_datetime(sessions['start_time_local'])
-    sessions['end_time_local'] = pd.to_datetime(sessions['end_time_local'])
-    sessions.drop('streams', axis=1, inplace=1)
-    
-    # insert sessions
-    sessions.to_sql('sessions', engine, index=False, if_exists='append')
+    # iterate through usernames
+    for ui, ur in users.iterrows():
+        # api quiery for session list
+        params = (('q[usernames]', ur['username']),)
+        response = requests.get('http://aircasting.org/api/sessions.json', params=params)
+        
+        # sessions dataframe
+        sessions = pd.DataFrame(response.json())
+        
+        # streams and measurements processing
+        for si, sr in sessions.iterrows():
+            # api query for session
+            response = requests.get('http://aircasting.org/api/sessions/%s.json' % (sr['id']))
+            
+            # process session
+            session = pd.Series(response.json())
+            session['created_at'] = pd.to_datetime(session['created_at'])
+            session['updated_at'] = pd.to_datetime(session['updated_at'])
+            session['start_time'] = pd.to_datetime(session['start_time'])
+            session['end_time'] = pd.to_datetime(session['end_time'])
+            session['start_time_local'] = pd.to_datetime(session['start_time_local'])
+            session['end_time_local'] = pd.to_datetime(session['end_time_local'])
+            session['last_measurement_at'] = pd.to_datetime(session['last_measurement_at'])
+            session.drop('streams', inplace=True)
+            session.drop('notes', inplace=True)
+            session.drop('measurements_received_count', inplace=True)
+            
+            # update user id if not in users table
+            if pd.isnull(ui):
+                pd.read_sql_query('UPDATE users SET id = %d WHERE username = \'%s\'' % (session['user_id'], ur['username']), engine)
+            
+            # insert into sql
+            pd.DataFrame().append(session, ignore_index=True).to_sql('sessions', engine, index=False, if_exists='append')
+                
+            # process streams
+            streams = pd.DataFrame(response.json()['streams']).transpose()
+            streams['session_id'] = sr['id']
+            streams.drop('measurements', axis=1, inplace=True)
+            streams.drop('size', axis=1, inplace=True)
+            
+            # insert into sql
+            streams.to_sql('streams', engine, index=False, if_exists='append')
+            
+            # iterate through streams
+            for s in response.json()['streams']:
+                # get stream
+                stream = response.json()['streams'][s]
+                
+                # process measurements
+                measurements = pd.DataFrame(stream['measurements'])
+                measurements['time'] = pd.to_datetime(measurements['time'])
+                measurements['stream_id'] = stream['id']
+                
+                # insert into sql
+                measurements.to_sql('measurements', engine, index='id', if_exists='append')
     
     # get sessions
-    sessions = pd.read_sql_query('SELECT * FROM sessions', engine, index_col='id', parse_dates=['start_time_local', 'end_time_local'])
+    #sessions = pd.read_sql_query('SELECT * FROM sessions', engine, index_col='id', parse_dates=['start_time_local', 'end_time_local'])
