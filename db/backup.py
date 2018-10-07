df669f1741dacd7cf56eb63bccf5bff255aed412
Dan Wahl <drwahl@gmail.com>
2018-10-07 17:29:08 -0500

    modifying backup script for low memory support

--- db/backup.py
+++ db/backup.py
@@ -6,6 +6,8 @@ import shutil
 import os
 import glob
 
+CHUNKSIZE = 10000
+
 if __name__ == '__main__':
     # generate error log
     logging.basicConfig(
@@ -27,39 +29,65 @@ if __name__ == '__main__':
     # tables to back up
     tables = ['users', 'sessions', 'streams', 'notes', 'measurements', 'neighborhoods', 'tracts', 'wards']
     
-    # back up each table
-    for table in tables:
+    # back up each table, beaking breaking into multiple files as necessary
+    for table in tables:     
         try:
-            df = pd.read_sql_query('SELECT * FROM %s' % (table), engine)
+            gen = pd.read_sql_query('SELECT * FROM %s' % (table), engine, chunksize=CHUNKSIZE)
         except sa.exc.SQLAlchemyError as e:
             logging.error(e)
             sys.exit(1)
         
+        i = 0
+        files = []
+        for df in gen:
+            try:
+                name = 'backups/tables/%s_%d.csv'% (table, i)
+                header = False if i else True
+                df.to_csv(name, index=False, header=header, encoding='utf-8')   
+            except:
+                e = sys.exc_info()
+                logging.error(str(e))
+                sys.exit(1)
+            
+            i += 1
+            files.append(name)
+        
+        # delete destination file, if exists
         try:
-            df.to_csv('backups/tables/%s.csv' % table, index=False, encoding='utf-8')
+            destination = 'backups/tables/%s.csv' % table
+            os.remove(destination)
+            os.system('touch %s' % destination)
+        except OSError:
+            pass
+        
+        # combine files
+        try:            
+            for f in files:
+                os.system('cat %s >> %s' % (f, destination))
+                os.remove(f)
         except:
-            e = sys.exc_info()[0]
-            logging.error(e)
+            e = sys.exc_info()
+            logging.error(str(e))
             sys.exit(1)
-    
+
     # create zip file
     try:
         name = 'backups/backup_%s' % (pd.Timestamp.now().strftime('%Y%m%d%H%M%S'))
         shutil.make_archive(name, 'zip', 'backups/tables')
     except:
-        e = sys.exc_info()[0]
-        logging.error(e)
+        e = sys.exc_info()
+        logging.error(str(e))
         sys.exit(1)
     
     # remove old zip files
     try:
         files = glob.glob('backups/*.zip')
         files.remove(name + '.zip')
-        for file in files:
-            os.remove(file)
+        for f in files:
+            os.remove(f)
     except:
-        e = sys.exc_info()[0]
-        logging.error(e)
+        e = sys.exc_info()
+        logging.error(str(e))
         sys.exit(1)
     
     logging.info('backup finished')
